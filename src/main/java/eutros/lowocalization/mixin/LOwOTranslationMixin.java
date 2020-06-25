ad8e7e590da5cd873f69ede96ef15dcd4e914e25
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-06-25 17:23:02 +0100

    Add mappings for reflection.

--- src/main/java/eutros/lowocalization/mixin/LOwOTranslationMixin.java
+++ src/main/java/eutros/lowocalization/mixin/LOwOTranslationMixin.java
@@ -1,9 +1,14 @@
 package eutros.lowocalization.mixin;
 
 import eutros.lowocalization.core.LOwOLanguageMap;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.client.resource.language.TranslationStorage;
 import net.minecraft.util.Language;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,10 +20,24 @@ import java.util.Map;
 @Mixin(Language.class)
 public class LOwOTranslationMixin {
 
-    private static final Field FIELD = FieldUtils.getField(TranslationStorage.class, "translations", true);
+    private static final MappingResolver mr = FabricLoader.getInstance().getMappingResolver();
+
+    @Nullable
+    private static final Field FIELD = FieldUtils.getField(TranslationStorage.class,
+            mr.mapFieldName("intermediary",
+                    "net.minecraft.class_1078",
+                    "field_5330",
+                    "Ljava/util/Map;"),
+            true);
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Inject(at = @At("HEAD"), method = "setInstance")
     private static void languageSetInstance(Language language, CallbackInfo ci) {
+        if(FIELD == null) {
+            LOGGER.warn("Could not set language map, field not found.");
+            return;
+        }
         if(language instanceof TranslationStorage) {
             try {
                 //noinspection unchecked
