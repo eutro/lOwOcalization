e81bb42221ab706bfad8cff12b1700b23ee7b18d
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-08-26 14:53:54 +0100

    Update to 1.16.2, use mixin for map injection.

--- src/main/java/eutros/lowocalization/core/LOwOcalization.java
+++ src/main/java/eutros/lowocalization/core/LOwOcalization.java
@@ -2,11 +2,9 @@ package eutros.lowocalization.core;
 
 import eutros.lowocalization.core.common.LOwOcalizer;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(LOwOcalization.MOD_ID)
@@ -16,16 +14,9 @@ public class LOwOcalization {
     public static final String NAME = "lOwOcalisati\u03C9n";
 
     public LOwOcalization() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener(this::clientSetup);
-        bus.addListener(LOwOConfig::onChange);
-
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, LOwOConfig.CLIENT_SPEC);
-    }
-
-    private void clientSetup(FMLClientSetupEvent evt) {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(LOwOConfig::onChange);
         MinecraftForge.EVENT_BUS.addListener(LOwOcalizer.INSTANCE::onLOwOcalizationEvent);
-        MinecraftForge.EVENT_BUS.register(new LOwOMapInjector());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, LOwOConfig.CLIENT_SPEC);
     }
 
 }
